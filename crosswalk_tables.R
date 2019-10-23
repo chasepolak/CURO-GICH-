ef9a6c7e195b1c53e040da811d71e23f752dd7ef
unknown <mcp36046@uga.edu>
2019-10-23 02:21:24 -0400

    updated to csvs

--- crosswalk_tables.R
+++ crosswalk_tables.R
@@ -1,22 +1,23 @@
 library(tidyverse)
 
 #Create a variable table for each community
-commerce_vars<-as.tibble(names(commerce_info_issues)) %>%
-  mutate(commerce_pres=1)
 monroe_vars<-as.tibble(names(monroe_info_issues)) %>%
   mutate(monroe_pres=1)
 gaines_vars<-as.tibble(names(gainesville_info_issues)) %>%
   mutate(gaines_pres=1)
-<<<<<<< HEAD
+hartwell_vars<-as.tibble(names(hartwell_info_issues)) %>%
+  mutate(hartwell_pres=1)
+commerce_vars<-as.tibble(names(commerce_info_issues)) %>%
+  mutate(commerce_pres=1)
+warrenton_vars<-as.tibble(names(warrenton_info_issues)) %>%
+  mutate(warrenton_pres=1)
 cochran_vars<-as.tibble(names(cochran_info_issues)) %>%
   mutate(cochran_pres=1)
-all_vars<-full_join(gaines_vars,monroe_vars,commerce_vars,cochran_vars)
-=======
-
-all_vars<-full_join(gaines_vars,monroe_vars)
->>>>>>> 0eb6496029281c5bba991d3faad21705ee5d5c3e
+millen_vars<-as.tibble(names(millen_info_issues)) %>%
+  mutate(millen_pres=1)
+###error shows up when I try to full join the vars tables
+all_vars<-full_join(monroe_vars,gaines_vars,hartwell_vars,commerce_vars,warrenton_vars,cochran_vars,millen_vars)
 write_csv(all_vars,"data/var_crosswalk.csv")
-
 
 #warrenton parcels
 warrenton_spacer<-substr(warrenton_issues[3,]$Parcel_No,4,7)
