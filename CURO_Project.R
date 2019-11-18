d42956555a888f6a5b27822c14cec64241a30029
unknown <mcp36046@uga.edu>
2019-11-18 18:52:21 -0500

    analyssis of surveys

--- CURO_Project.R
+++ CURO_Project.R
@@ -10,7 +10,7 @@ record_times<-read_csv("data/record_times_all.csv") %>%
   group_by(parcel_num,primary_city) %>%
   summarise(record_id=first(record_id),
             date=first(date))
-commerce_issues <- read_csv("data/Commerce/Commerce_shiny/Data/parcel_points.csv")
+commerce_issues <- read_csv("data/commerce_parcel_points.csv")
 commerce_owner <- read_excel("data/commerce owner.xlsx")
 commerce_proprty <- read_excel("data/commerce proprty.xlsx")
 Gainesville_issues_and_info <- read_excel("data/Gainesville issues and info.xlsx")
@@ -77,9 +77,12 @@ gainesville_info_issues<-Gainesville_issues_and_info%>%
   mutate("primary_city"="gainesville") %>%
   left_join(gainesville_latlong)
 ###Combine hartwell
-hartwell_info_issues<-hartwell_issues_and_owner_correct_version
-hartwell_info_issues<-hartwell_info_issues%>%
-  mutate("primary_city"="hartwell")
+hartwell_info_issues_new<-hartwell_issues_and_owner_correct_version
+hartwell_info_issues<-hartwell_info_issues_new%>%
+  mutate("primary_city"="hartwell")%>%
+  rename(condition_1=condition)%>%
+  rename(condition=condition__1)
+  
 ###Combine millen
 millen_info_issues<-millen_issues %>%
   rename(Parcel_No=Parcel_ID) %>%
@@ -87,7 +90,10 @@ millen_info_issues<-millen_issues %>%
   left_join(millen_owner)%>%
   #left_join(millen_info) %>%
   mutate("primary_city"="millen")%>%
-  left_join(millen.home.exempt)
+  left_join(millen_home_exempt)
+millen_home_exempt_fixed<-millen_home_exempt%>%
+  rename(parcel_num=Parcel_No)%>%
+  rename(homeexempt=HOMEEXEMPT)
 ###Combine monroe
 monroe_info_issues<-monroe_issues%>%
   left_join(monroe_info,by="Parcel_No") %>%
@@ -100,6 +106,9 @@ cochran_info_issues<-cochran_issues%>%
   left_join(Cochran_info) %>%
   mutate("primary_city"="cochran")%>%
   left_join(cochran_home_exempt)
+cochran_exempt_fixed<-cochran_home_exempt%>%
+  rename(parcel_num=Parcel_No)%>%
+  rename(homeexempt=HOMEEXEMPT)
 ###Combine Warrenton, need help on fizxing excel
 warrenton_spacer<-substr(warrenton_issues[3,]$parcel_num,4,7)
 warrenton_issues<-warrenton_issues %>%
@@ -108,7 +117,7 @@ warrenton_issues<-warrenton_issues %>%
 #head(warrenton_issues$Parcel_No)
 warrenton_information <- warrenton_information %>%
   select(-Parcel_No) %>%
-  rename(Parcel_No=Parcel_Number)
+  rename(Parcel_No=Parcel_Num)
 warrenton_info_issues<-warrenton_issues%>%
   left_join(warrenton_information) %>%
   mutate(primary_city="warrenton")
@@ -149,7 +158,8 @@ millen_data_cw<-millen_info_issues %>%
   gather(-spreadid,key="var",value="value") %>%
   right_join(s1_cw %>% filter(millen==1)) %>%
   select(-var,-millen:-monroe) %>%
-  spread(universal,value) 
+  spread(universal,value)%>%
+  left_join(millen_home_exempt_fixed)
 
 gainesville_info_issues$spreadid<-1:nrow(gainesville_info_issues)
 gainesville_data_cw<-gainesville_info_issues %>%
@@ -190,6 +200,7 @@ s1_data_num<-s1_data %>%
 
 write_csv(s1_data_num,"s1_data.csv")
 
+
 #Version 2 data
 s2_cw<-read_csv("data/var_s2.csv")
 
@@ -207,7 +218,8 @@ cochran_data_cw<-cochran_info_issues %>%
   right_join(s2_cw %>% filter(cochran==1)) %>%
   select(-var,-hartwell:-warrenton) %>%
   spread(universal,value) %>%
-  select(-spreadid)
+  select(-spreadid)%>%
+  left_join(cochran_exempt_fixed)
 
 commerce_info_issues$spreadid<-1:nrow(commerce_info_issues)
 commerce_data_cw<-commerce_info_issues %>%
@@ -228,8 +240,17 @@ warrenton_data_cw<-warrenton_info_issues %>%
 
 
 #Combine s2 data
-s2_data<-bind_rows(hartwell_data_cw,cochran_data_cw,commerce_data_cw,
-                   warrenton_data_cw) %>%
+s2_data<-bind_rows(hartwell_data_cw,cochran_data_cw,commerce_data_cw,warrenton_data_cw) %>%
+  filter(condition!="NA")%>%
+  count(primary_city,owncity) %>%
+  filter(is.na(owncity)==FALSE) %>%
+  mutate(owncity1=tolower(owncity),
+         primcity1=tolower(primary_city),
+         citymatch=if_else(owncity1==primcity1,0,1)) %>%
+  select(primary_city,owncity,citymatch) %>%
+  right_join(s2_data) %>%
+  filter(is.na(owncity)==FALSE)
+###this part not working when added
   mutate(survey="v2") %>%
   left_join(record_times) %>%
   mutate(prop_add=if_else(is.na(address_add)==TRUE,prop_add,address_add)) %>%
@@ -253,3 +274,4 @@ write_csv(s2_data,"s2_data.csv")
 allsurvey_data<-bind_rows(s1_data_num %>% mutate(ownzip=as.character(ownzip)),
                           s2_data %>% mutate(x=as.character(x),y=as.character(y))) 
 write_csv(allsurvey_data,"allsurvey_data.csv")
+
